0d369a715044b46e3a851d81587588f2855b2f31
ParkYoungHo <enough6157@naver.com>
2025-02-02 14:54:44 +0900

    Buidl 1.3.9 인디케이터 추가

---
+++ lib/utils/yh_indicator.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:yh_design_system/atoms/color/colors.dart';
+
+final class YHIndicator {
+  static OverlayEntry? _indicator;
+
+  static void show(BuildContext? context, Widget? child) {
+    // 앱이 실행중인 상태라면 null일 수가 없지만,
+    // 위젯 트리에 붙지 않은 상태일 수 있기에 null을 반환할 수 있음.
+    // BuildContext? context = navigatorKey.currentContext;
+
+    if (context == null) return;
+
+    if (_indicator == null) {
+      _indicator = OverlayEntry(
+          builder: (_) => Center(
+              child: child ??
+                  CircularProgressIndicator(color: YHColor.primary.color)));
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Overlay.of(context).insert(_indicator!);
+      });
+    }
+  }
+
+  static void hide() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_indicator != null) {
+        _indicator?.remove();
+        _indicator = null;
+      }
+    });
+  }
+}
