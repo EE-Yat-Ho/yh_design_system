72c17c32de666bdc7dcdf87a23c524304027a523
ParkYoungHo <enough6157@naver.com>
2025-02-10 09:40:23 +0900

    Build: 1.4.5 Gray 색 정리

--- lib/atoms/color/colors.dart
+++ lib/atoms/color/colors.dart
@@ -36,13 +36,23 @@ class YHColor {
 
   static const YHColor gray50 =
       YHColor(color: Color.fromRGBO(249, 249, 249, 1));
-
   static const YHColor gray100 =
-      YHColor(color: Color.fromRGBO(244, 244, 246, 1));
+      YHColor(color: Color.fromRGBO(231, 231, 231, 1));
   static const YHColor gray200 =
-      YHColor(color: Color.fromRGBO(237, 237, 237, 1));
+      YHColor(color: Color.fromRGBO(229, 229, 229, 1));
+  static const YHColor gray300 =
+      YHColor(color: Color.fromRGBO(217, 217, 217, 1));
   static const YHColor gray400 =
       YHColor(color: Color.fromRGBO(209, 209, 209, 1));
+  static const YHColor gray500 =
+      YHColor(color: Color.fromRGBO(198, 198, 198, 1));
+  static const YHColor gray600 =
+      YHColor(color: Color.fromRGBO(180, 180, 180, 1));
+  static const YHColor gray700 =
+      YHColor(color: Color.fromRGBO(141, 141, 141, 1));
+  static const YHColor gray800 =
+      YHColor(color: Color.fromRGBO(109, 109, 109, 1));
+  static const YHColor gray900 = YHColor(color: Color.fromRGBO(83, 83, 83, 1));
 
   static const YHColor white =
       YHColor(color: Color.fromARGB(255, 255, 255, 255));
@@ -56,6 +66,8 @@ class YHColor {
 
   static const YHColor optionsBG =
       YHColor(color: Color.fromARGB(255, 220, 223, 228));
+  static const YHColor grayBG =
+      YHColor(color: Color.fromRGBO(244, 244, 246, 1));
   static const YHColor note = YHColor(color: Color.fromARGB(255, 255, 146, 10));
 
   const YHColor({
