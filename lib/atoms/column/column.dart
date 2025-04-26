75f33575554a6b99f681b20f8d5c229617179844
park.youngho <enough6157@naver.com>
2025-04-26 20:34:38 +0900

    Column

---
+++ lib/atoms/column/column.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class YHColumn extends StatelessWidget {
+  const YHColumn({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.mainAxisSize = MainAxisSize.max,
+    required this.children,
+    this.padding,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final List<Widget> children;
+  final EdgeInsets? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final column = Column(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      children: children,
+    );
+    if (padding != null) {
+      return Padding(
+        padding: padding!,
+        child: column,
+      );
+    }
+    return column;
+  }
+}
