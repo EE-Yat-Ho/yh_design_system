4f45b490fb57de2fcd149a8339fcd38df06f7fea
park.youngho <enough6157@naver.com>
2025-05-17 15:02:31 +0900

    YHColumn에 spacing 추가

--- lib/atoms/column/column.dart
+++ lib/atoms/column/column.dart
@@ -8,6 +8,7 @@ class YHColumn extends StatelessWidget {
     this.mainAxisSize = MainAxisSize.max,
     required this.children,
     this.padding,
+    this.spacing = 0,
   });
 
   final MainAxisAlignment mainAxisAlignment;
@@ -15,13 +16,16 @@ class YHColumn extends StatelessWidget {
   final MainAxisSize mainAxisSize;
   final List<Widget> children;
   final EdgeInsets? padding;
+  final double spacing;
 
   @override
   Widget build(BuildContext context) {
     final column = Column(
+      key: key,
       mainAxisAlignment: mainAxisAlignment,
       crossAxisAlignment: crossAxisAlignment,
       mainAxisSize: mainAxisSize,
+      spacing: spacing,
       children: children,
     );
     if (padding != null) {
