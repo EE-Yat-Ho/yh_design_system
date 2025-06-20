05e381fa02cb62e47811cc2dd887bb393b3c399f
park.youngho <enough6157@naver.com>
2025-06-20 16:21:58 +0900

    tab

--- lib/organisms/tabbar/tab.dart
+++ lib/organisms/tabbar/tab.dart
@@ -4,16 +4,19 @@ import 'package:yh_design_system/atoms/font/fonts.dart';
 class YHTab extends StatelessWidget {
   const YHTab({
     super.key,
+    this.height = 40,
     required this.text,
     required this.font,
   });
 
   final String text;
   final YHFont font;
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
     return Tab(
+      height: height,
       child: Text(
         text,
         style: TextStyle(fontWeight: font.fontWeight, fontSize: font.fontSize),
