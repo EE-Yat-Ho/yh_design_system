5b3fd9b79b0df8d6ec8160b4b54466f825454e51
park.youngho <enough6157@naver.com>
2025-07-10 15:44:36 +0900

    title

--- lib/organisms/title/title.dart
+++ lib/organisms/title/title.dart
@@ -3,20 +3,22 @@ import 'package:yh_design_system/atoms/color/colors.dart';
 import 'package:yh_design_system/atoms/text/text.dart';
 import 'package:yh_design_system/atoms/font/fonts.dart';
 
-class YHTitle extends StatelessWidget {
+final class YHTitle extends StatelessWidget {
   const YHTitle({
     super.key,
     required this.title,
+    this.padding = const EdgeInsets.all(8.0),
   });
 
   final String title;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topLeft,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: padding,
         child: YHText(
           text: title,
           font: YHFont.bold24,
