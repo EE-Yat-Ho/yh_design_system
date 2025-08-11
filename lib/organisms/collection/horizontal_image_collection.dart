aba213339ded29cc630f1711b3975898cb6fe2ca
park.youngho <enough6157@naver.com>
2025-08-11 12:52:55 +0900

    image detail

--- lib/organisms/collection/horizontal_image_collection.dart
+++ lib/organisms/collection/horizontal_image_collection.dart
@@ -35,7 +35,7 @@ final class HorizontalImageCollection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: itemHeight, // 컬렉션 높이
+      height: itemHeight + (showSelectedBorder ? borderWidth * 2 : 0), // 컬렉션 높이
       child: ListView.builder(
         padding: contentInsets,
         scrollDirection: Axis.horizontal, // 가로 스크롤 설정
@@ -70,8 +70,8 @@ final class HorizontalImageCollection extends StatelessWidget {
     return Image.file(
       image.file,
       fit: image.fit,
-      width: itemWidth - (showBorder ? borderWidth * 2 : 0),
-      height: itemHeight - (showBorder ? borderWidth * 2 : 0),
+      width: itemWidth, // - (showBorder ? borderWidth * 2 : 0),
+      height: itemHeight, //- (showBorder ? borderWidth * 2 : 0),
       errorBuilder: (context, error, stackTrace) {
         debugPrint("🚨🏞️ 이미지 표시 실패 error: $error, stackTrace: $stackTrace");
         // 이미지 표시 실패 시 대체 이미지
