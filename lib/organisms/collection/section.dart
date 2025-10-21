459dfe637c829f7ca348a78f484feef601847a29
park.youngho <enough6157@naver.com>
2025-10-21 12:43:27 +0900

    section

--- lib/organisms/collection/section.dart
+++ lib/organisms/collection/section.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:yh_design_system/atoms/color/colors.dart';
+import 'package:yh_design_system/atoms/font/fonts.dart';
+import 'package:yh_design_system/atoms/text/text.dart';
 import 'package:yh_design_system/organisms/collection/cell.dart';
 
 final class YHSection {
@@ -5,4 +9,36 @@ final class YHSection {
   final List<YHCell> cells;
 
   YHSection(this.title, this.cells);
+}
+
+// 각종 리스트에서 사용할 섹션용 위젯
+final class YHSectionWidget extends StatelessWidget {
+  const YHSectionWidget({
+    super.key,
+    required this.section,
+    this.font = YHFont.regular16,
+    this.color,
+    this.padding = const EdgeInsets.fromLTRB(4, 0, 4, 0),
+  });
+
+  final YHSection section;
+  final YHFont font;
+  final Color? color;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("🏗️ $this build");
+    var text = YHText(
+      text: section.title,
+      font: font,
+      color: color ?? YHColor.textDefault,
+    );
+
+    return ListTile(
+      dense: true,
+      leading: text,
+      contentPadding: padding,
+    );
+  }
 }
