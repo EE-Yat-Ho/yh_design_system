f2124849a14fac46e483cc4fc684e4438a59cad0
park.youngho <enough6157@naver.com>
2025-10-21 12:36:43 +0900

    section

--- lib/organisms/collection/setting_list/setting_section_card.dart
+++ lib/organisms/collection/setting_list/setting_section_card.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:yh_design_system/atoms/color/colors.dart';
-import 'package:yh_design_system/atoms/text/text.dart';
-import 'package:yh_design_system/atoms/font/fonts.dart';
-import 'package:yh_design_system/organisms/collection/setting_list/setting_list.dart';
+// import 'package:flutter/material.dart';
+// import 'package:yh_design_system/atoms/color/colors.dart';
+// import 'package:yh_design_system/atoms/text/text.dart';
+// import 'package:yh_design_system/atoms/font/fonts.dart';
+// import 'package:yh_design_system/organisms/collection/setting_list/setting_list.dart';
 
-final class SettingSectionCard extends StatelessWidget {
-  const SettingSectionCard({super.key, required this.section});
+// final class SettingSectionCard extends StatelessWidget {
+//   const SettingSectionCard({super.key, required this.section});
 
-  final SettingSection section;
+//   final SettingSection section;
 
-  @override
-  Widget build(BuildContext context) {
-    debugPrint("🏗️ $this build");
-    var text = YHText(
-        text: section.title,
-        font: YHFont.regular16,
-        color: YHColor.textDefault);
+//   @override
+//   Widget build(BuildContext context) {
+//     debugPrint("🏗️ $this build");
+//     var text = YHText(
+//         text: section.title,
+//         font: YHFont.regular16,
+//         color: YHColor.textDefault);
 
-    return ListTile(
-      dense: true,
-      leading: text,
-      contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
-    );
-  }
-}
+//     return ListTile(
+//       dense: true,
+//       leading: text,
+//       contentPadding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+//     );
+//   }
+// }
