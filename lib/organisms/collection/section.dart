dd98f444ba782a83b19fa61d36b57d31164af427
park.youngho <enough6157@naver.com>
2025-10-21 12:35:06 +0900

    section

---
+++ lib/organisms/collection/section.dart
@@ -0,0 +1,8 @@
+import 'package:yh_design_system/organisms/collection/cell.dart';
+
+final class YHSection {
+  final String title;
+  final List<YHCell> cells;
+
+  YHSection(this.title, this.cells);
+}
