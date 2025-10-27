05d924104b238d6b6cb89a93c2d05eee845a6f37
park.youngho <enough6157@naver.com>
2025-10-27 21:14:25 +0900

    포인트를 위해 로그인하는 다이얼로그

--- lib/organisms/dialog/suggest_login_dialog.dart
+++ lib/organisms/dialog/suggest_login_dialog.dart
@@ -3,24 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:yh_design_system/atoms/image/images.dart';
 import 'package:yh_design_system/organisms/dialog/dialog.dart';
 
-Future<dynamic> showSuggestLoginDialog(
+Future<bool> showSuggestLoginDialog(
   BuildContext context,
-  void Function() onConfirm,
-  void Function() onCancel,
 ) async {
-  return showDialog(
-      context: context,
-      builder: (BuildContext innerContext) {
-        return YHDialog(
-          image: YHImage.char_question_mark,
-          buttonDirection: ButtonDirection.vertical,
-          columnCrossAxisAlignment: CrossAxisAlignment.center,
-          text: "yh_design_system.dialog.suggest_login.title".tr(),
-          titleAlign: TextAlign.center,
-          onConfirm: () => onConfirm(),
-          confirmText: "yh_design_system.dialog.suggest_login.confirm".tr(),
-          onCancel: () => onCancel(),
-          cancelText: "yh_design_system.dialog.suggest_login.cancel".tr(),
-        );
-      });
+  return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext innerContext) {
+            return YHDialog(
+              image: YHImage.char_question_mark,
+              buttonDirection: ButtonDirection.vertical,
+              columnCrossAxisAlignment: CrossAxisAlignment.center,
+              text: "yh_design_system.dialog.suggest_login.title".tr(),
+              titleAlign: TextAlign.center,
+              onConfirm: () => Navigator.pop(innerContext, true),
+              confirmText: "yh_design_system.dialog.suggest_login.confirm".tr(),
+              onCancel: () => Navigator.pop(innerContext, false),
+              cancelText: "yh_design_system.dialog.suggest_login.cancel".tr(),
+            );
+          }) ??
+      false;
 }
