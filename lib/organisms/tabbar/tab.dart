38799466468f65f1535e90ad113fd72e5a8f115a
park.youngho <enough6157@naver.com>
2025-11-13 20:35:25 +0900

    YHTab reddot

--- lib/organisms/tabbar/tab.dart
+++ lib/organisms/tabbar/tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yh_design_system/atoms/color/colors.dart';
+import 'package:yh_design_system/atoms/decoration/yh_red_dot.dart';
 import 'package:yh_design_system/atoms/text/text.dart';
 import 'package:yh_design_system/atoms/font/fonts.dart';
 
@@ -10,21 +11,47 @@ final class YHTab extends StatelessWidget {
     required this.text,
     this.color,
     this.font = YHFont.regular16,
+    this.redDot = false,
+    this.redDotInset = 4,
+    this.redDotSize = 6,
   });
 
   final String text;
   final Color? color;
   final YHFont font;
   final double? height;
+  final bool redDot;
+  final double redDotInset;
+  final double redDotSize;
 
   @override
   Widget build(BuildContext context) {
-    return Tab(
-        height: height,
-        child: YHText(
-          text: text,
-          font: font,
-          color: color ?? YHColor.textSub,
-        ));
+    final tab = Tab(
+      height: height,
+      child: YHText(
+        text: text,
+        font: font,
+        color: color ?? YHColor.textSub,
+      ),
+    );
+
+    if (redDot) {
+      return Stack(
+        children: [
+          tab,
+          Positioned(
+            top: redDotInset,
+            right: redDotInset,
+            child: Container(
+              width: redDotSize,
+              height: redDotSize,
+              decoration: redDotDecoration,
+            ),
+          ),
+        ],
+      );
+    } else {
+      return tab;
+    }
   }
 }
