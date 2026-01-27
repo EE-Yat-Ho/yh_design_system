af9e011ed59e7c678f27dff71749c80e3d59c04c
park.youngho <enough6157@naver.com>
2026-01-27 16:16:26 +0900

    error dialog

--- lib/organisms/dialog/error_dialog.dart
+++ lib/organisms/dialog/error_dialog.dart
@@ -1,10 +1,19 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:yh_design_system/organisms/dialog/dialog.dart';
 
-Future<dynamic> showErrorDialog(BuildContext context, Object exception) async {
+Future<dynamic> showErrorDialog(
+  BuildContext context,
+  Object exception,
+  VoidCallback onConfirm,
+) async {
   return showDialog(
       context: context,
       builder: (_) {
-        return YHDialog(text: exception.toString(), onConfirm: () {});
+        return YHDialog(
+          text: 'yh_design_system.dialog.error.title'.tr(),
+          subText: exception.toString(),
+          onConfirm: onConfirm,
+        );
       });
 }
